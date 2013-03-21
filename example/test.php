dea4ca359f9f6bef0e0d54686f22762b65cac3f3
hfcorriez <hfcorriez@gmail.com>
2013-03-22 00:04:37 +0800

    Improves and fix

--- example/test.php
+++ example/test.php
@@ -4,8 +4,10 @@ $loader = require dirname(__DIR__) . '/lib/CodeGun/ArgParser.php';
 
 $arg_parser = new \CodeGun\ArgParser();
 $arg_parser->add('a', array('help' => 'a long time with he happy', 'enum' => array(
-    'go' => 'do some thing with go'
 )));
-$arg_parser->add('--long|-l', array('help' => 'long arguments', 'enum' => array('a', 'b')));
+$arg_parser->add(array('--long', '-l'), array('help' => 'long arguments', 'enum' => array('a', 'b')));
 $args = $arg_parser->parse();
+
+var_dump($args, $arg_parser->error());
+
 echo $arg_parser->help();
